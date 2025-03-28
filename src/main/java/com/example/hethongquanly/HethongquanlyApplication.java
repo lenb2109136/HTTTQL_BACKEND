de19233066dffe22be4f49bef2ef40236f18f207
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-03-28 10:43:18 +0700

    COMMIT

---
+++ src/main/java/com/example/hethongquanly/HethongquanlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hethongquanly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HethongquanlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HethongquanlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hethongquanly/HethongquanlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hethongquanly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HethongquanlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
